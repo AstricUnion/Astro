0648852b457ceee46eeb1fbbdbb2d48caa979958
maxobur0001 <msblock09@gmail.com>
2026-07-09 20:25:47 +0300

    model test added

---
+++ modeltest.lua
@@ -0,0 +1,14 @@
+---@include astronew/baseincludes.lua
+---@include astronew/models/astroscout.lua
+
+require("astronew/baseincludes.lua")
+require("astronew/models/astroscout.lua")
+
+
+if SERVER then
+    local ch = chip()
+    local mdl = model.create("astroscout")
+    mdl:setPos(ch:getPos())
+    mdl:setAngles(ch:getAngles())
+    mdl:setParent(ch)
+end
