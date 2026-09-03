5b660a75981774af59d0bf48c343d01eab2c1356
maxobur0001 <msblock09@gmail.com>
2026-09-04 00:15:09 +0300

    Blender plugin changes to export frames

--- animations/blender_plugin.py
+++ animations/blender_plugin.py
@@ -25,7 +25,7 @@ propertyName = {
 
 
 class GRAPH_OT_fcurve_to_starfall(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
-    """FCurve to StarfallEx"""
+    """FCurve to StarfallEx (for tween)"""
 
     bl_idname = "graph.fcurve_to_starfall"
     bl_label = "Export tween"
@@ -103,6 +103,58 @@ class GRAPH_OT_fcurve_to_starfall(bpy.types.Operator, bpy_extras.io_utils.Export
 
         return {"FINISHED"}
 
+class VIEW3D_OT_frames_to_starfall(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
+    """Frames to StarfallEx (for model library)"""
+
+    bl_idname = "view3d.frames_to_starfall"
+    bl_label = "Export frames"
+    bl_options = {"REGISTER", "UNDO"}
+    filename_ext = ".txt"
+
+    save_path: bpy.props.StringProperty(name="Path to export", subtype='FILE_PATH')
+
+    @staticmethod
+    def getVector(vec):
+        copied = vec * 39.37008
+        return "Vector({:g}, {:g}, {:g})".format(round(copied.z, 5), round(copied.x, 5), round(copied.y, 5))
+    
+    @staticmethod
+    def getAngle(ang):
+        copied = ang.copy()
+        return "Angle({:g}, {:g}, {:g})".format(math.degrees(copied.x), math.degrees(copied.y), math.degrees(copied.z))
+        
+
+    def execute(self, context):
+        obj = context.object
+        ad = obj.animation_data
+        if not ad:
+            self.report({"ERROR"}, "There's no animation data in object")
+            return {"CANCELLED"}
+        anim_len = int(ad.action.frame_range.y)
+        keyframes = []
+
+        for posebone in obj.pose.bones: posebone.matrix_basis.identity()
+        
+        for i in range(anim_len):
+            bpy.context.window_manager.progress_update(i / anim_len)
+            bpy.context.scene.frame_set(i)
+
+            keyframe = []
+            depsgraph = bpy.context.evaluated_depsgraph_get()
+            evaluated_armature = obj.evaluated_get(depsgraph)
+            for posebone in evaluated_armature.pose.bones:
+                if posebone.name == "origin": continue
+                PoseMatrix = posebone.matrix
+                if posebone.parent is not None:
+                    PoseMatrix = posebone.parent.matrix.inverted() @ PoseMatrix
+                keyframe.append("    {{\"{}\", {}, {}}}".format(posebone.name, self.getVector(PoseMatrix.to_translation()), self.getAngle(PoseMatrix.to_euler('ZXY'))))
+            keyframes.append("{" + (",\n".join(keyframe)) + "}")
+
+        f = open(self.filepath, "w", encoding='utf-8')
+        f.write("{" + (",\n".join(keyframes)) + "}")
+        f.close()
+
+        return {"FINISHED"}
     
 class VIEW3D_OT_model_from_gmod(bpy.types.Operator, bpy_extras.io_utils.ImportHelper):
     """Import model from library"""
@@ -216,12 +268,14 @@ class VIEW3D_PT_starfall_panel(bpy.types.Panel):  # class naming convention ‘C
     def draw(self, context):
         row = self.layout.row()
         row.operator("view3d.model_from_gmod", text="Import model from StarfallEx")
+        row.operator("view3d.frames_to_starfall", text="Export StarfallEx frames")
 
 
 def register():
     bpy.utils.register_class(GRAPH_PT_starfall_panel)
     bpy.utils.register_class(VIEW3D_PT_starfall_panel)
     bpy.utils.register_class(GRAPH_OT_fcurve_to_starfall)
+    bpy.utils.register_class(VIEW3D_OT_frames_to_starfall)
     bpy.utils.register_class(VIEW3D_OT_model_from_gmod)
 
 
@@ -229,6 +283,7 @@ def unregister():
     bpy.utils.unregister_class(GRAPH_PT_starfall_panel)
     bpy.utils.unregister_class(VIEW3D_PT_starfall_panel)
     bpy.utils.unregister_class(GRAPH_OT_fcurve_to_starfall)
+    bpy.utils.unregister_class(VIEW3D_OT_frames_to_starfall)
     bpy.utils.unregister_class(VIEW3D_OT_model_from_gmod)
 
 
